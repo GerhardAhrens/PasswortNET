5c544fa3ef477193050b49dd58c1b59f85adbf97
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-02-05 20:53:25 +0100

    *

--- Views/ContentControls/LoginUC.xaml.cs
+++ Views/ContentControls/LoginUC.xaml.cs
@@ -20,6 +20,18 @@
             this.DataContext = this;
         }
 
+        public string LoginUser
+        {
+            get => base.GetValue<string>();
+            set => base.SetValue(value);
+        }
+
+        public string LoginPasswort
+        {
+            get => base.GetValue<string>();
+            set => base.SetValue(value);
+        }
+
         public override void InitCommands()
         {
             base.CmdAgg.AddOrSetCommand("LoginCommand", new RelayCommand(p1 => this.LoginHandler(p1), p2 => true));
@@ -27,6 +39,7 @@
 
         private void LoginHandler(object p1)
         {
+            var aa = this.passwordBox.Password;
             base.EventAgg.Publish<ChangeViewEventArgs>(new ChangeViewEventArgs
             {
                 Sender = this.GetType().Name,
