8beee45ab377621c5459331db0a2a25f74bf18c7
Gerhard Ahrens <gerhard.ahrens@lifeprojects.de>
2025-02-16 19:03:02 +0100

    *

---
+++ DataRepository/DatabaseBackup.cs
@@ -0,0 +1,38 @@
+/*
+ * <copyright file="DatabaseBackup.cs" company="Lifeprojects.de">
+ *     Class: DatabaseBackup
+ *     Copyright © Lifeprojects.de 2025
+ * </copyright>
+ *
+ * <author>Gerhard Ahrens - Lifeprojects.de</author>
+ * <email>gerhard.ahrens@lifeprojects.de</email>
+ * <date>16.02.2025 19:01:17</date>
+ * <Project>CurrentProject</Project>
+ *
+ * <summary>
+ * Beschreibung zur Klasse
+ * </summary>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by the Free Software Foundation, 
+ * either version 3 of the License, or (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,but WITHOUT ANY WARRANTY; 
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.You should have received a copy of the GNU General Public License along with this program. 
+ * If not, see <http://www.gnu.org/licenses/>.
+*/
+
+namespace PasswortNET.DataRepository
+{
+    using ModernBaseLibrary.Core;
+
+    public class DatabaseBackup : DisposableCoreBase
+    {
+		/// <summary>
+		/// Initializes a new instance of the <see cref="DatabaseBackup"/> class.
+		/// </summary>
+		public DatabaseBackup()
+		{
+		}
+	}
+}
